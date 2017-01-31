f89a4e6d5ef90f15d00ceecc7de512454abc851c
Charles LeDoux <charles.a.ledoux@gmail.com>
2017-01-30 21:56:53 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sortphotos',
-    version='1.0',
+    version='1.1',
     description='Organizes photos and videos into folders using date/time information ',
     author='Andrew Ning',
     packages=find_packages(),
